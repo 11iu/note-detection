d98ffdd3c1b1b76480315d4a2c4d209d4efcec6e
11iu <liu.louis91@gmail.com>
2024-01-30 22:02:34 -0700

    initial commit

---
+++ detect_with_yolo.py
@@ -0,0 +1,11 @@
+from ultralytics import YOLO
+
+# Load a pretrained YOLOv8n model
+model = YOLO('note-model.pt')
+
+# Run inference on an image
+results = model('note3.jpg')  # results list
+
+# View results
+for r in results:
+    print(r.boxes)  # print the Boxes object containing the detection bounding boxes
